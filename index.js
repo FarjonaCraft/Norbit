f7f795f1c4127150bbd1da15a574ab48358286d7
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 08:07:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const config = require("./config.json")
 const bot = new Discord.Client();
 const fs = require("fs");
 bot.commands = new Discord.Collection();
-if(config.token === "setmeplease") return console.log("Set your token up! Go to https://www.discordapp.com/developers and generate a token from a bot user.");
 
 fs.readdir("./commands/", (err, files) => {
 
