acb76be06e32efc104540a7ce36be2fa99f14254
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 20:02:03 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,13 +8,21 @@ module.exports.run = async (bot, message, args) => {
   .setColor("RED");
   if(!message.guild.me.hasPermission("SEND_MESSAGES")) return message.channel.send(Idonthavepermission);
 let sicon = message.guild.iconURL;
+  
+let membercount =  message.guild.members.filter(member => !member.user.bot, o => o.presence.status == 'online').size
+let membercountonl = message.guild.members.filter(o => o.presence.status == 'online').size
+let membercountidle = message.guild.members.filter(o => o.presence.status == 'idle').size
+let membercountdnd = message.guild.members.filter(o => o.presence.status == 'dnd').size
+let membercountffli = message.guild.members.filter(o => o.presence.status == 'invisible').size
+let botcountonl = message.guild.members.filter(member => member.user.bot).size
+let membercountinv = membercountonl + membercountidle + membercountdnd + membercountffli + botcountonl
 
 const embed = new Discord.RichEmbed()
     .setTitle(`${message.guild.name}'s information`, true)   
     .addField("Server Name", `${message.guild.name}`, true)
     .setColor("GREEN")
     .setThumbnail(sicon)
-    .addField('Users', `${bot.user.size}`, true)
+    .addField('Users', `${membercountinv}`, true)
     .addField('Guild Owner', `${message.guild.owner.user.username}`, true)
     .addField('Verification Level', `${message.guild.verificationLevel}`, true)
     .addField('Owner ID', `${message.guild.owner.id}`, true)
