b6a615f778f1a6a674e8f6c43e5195f3bbe0d9c9
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 17:51:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,47 +31,7 @@ bot.on("ready", async () => {
       bot.user.setPresence({ game: { name: status}, status: 'online'});
       bot.user.setPresence({ activity: { name: status}, status: 'online'});
     }, 10000)
-      bot.on("message", async message => {
-      let user = message.mentions.users.first() || message.author;
 
-      if(!user) return message.channel.send("User is not defined in this channel!");
-      let embedcoins = new Discord.RichEmbed()
-      .setAuthor(`${user.tag}'s Information`)
-      .setThumbnail(user.displayAvatarURL)
-      .setColor(`#5eff98`)
-      .addField('Username', user.username, true)
-      .addField('Discrim', user.discriminator, true)
-      .addField('Status', user.presence.status, true)
-      .addField('Bot?', user.bot, true)
-      .addField('Account created at', user.createdAt, false)
-      .setThumbnail(user.displayAvatarURL)
-      .setTimestamp()
-      .setFooter(`Requested by: ${message.author.username}`, message.author.avatarURL);
-
-      if(message.content.startsWith (">userinfo")) {
-      message.channel.send(embedcoins)
-      } 
-  
-      let user = message.mentions.users.first() || message.author;
-
-      if(!user) return message.channel.send("User is not defined in this channel!");
-      let embedcoins = new Discord.RichEmbed()
-      .setAuthor(`${user.tag}'s Information`)
-      .setThumbnail(user.displayAvatarURL)
-      .setColor(`#5eff98`)
-      .addField('Username', user.username, true)
-      .addField('Discrim', user.discriminator, true)
-      .addField('Status', user.presence.status, true)
-      .addField('Bot?', user.bot, true)
-      .addField('Account created at', user.createdAt, false)
-      .setThumbnail(user.displayAvatarURL)
-      .setTimestamp()
-      .setFooter(`Requested by: ${message.author.username}`, message.author.avatarURL);
-
-      if(message.content.startsWith (">userinfo")) {
-      message.channel.send(embedcoins)
-      } 
-  });
 
 
 bot.on("message", async message => {
