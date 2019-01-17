4cda3393d004ecb061e74fd145367b3706780d17
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 15:23:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ bot.on("message", async message => {
   let prefix = config.prefix;
 
   //checks if message contains a command and runs it
-
+  if(commandfile) commandfile.run(bot, message, args);
   let commandfile = bot.commands.get(command.slice(prefix.length));
   if(commandfile) commandfile.run(bot, message, args);
 
