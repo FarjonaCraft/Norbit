db4c25792acef37764685092f9e1545a4678a52e
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 18:30:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,47 @@ bot.on("ready", async () => {
 
     }, 10000)
 
+  
 
+      let user = message.mentions.users.first() || message.author;
+
+
+
+      if(!user) return message.channel.send("User is not defined in this channel!");
+
+      let embedcoins = new Discord.RichEmbed()
+
+      .setAuthor(`${user.tag}'s Information`)
+
+      .setThumbnail(user.displayAvatarURL)
+
+      .setColor(`#5eff98`)
+
+      .addField('Username', user.username, true)
+
+      .addField('Discrim', user.discriminator, true)
+
+      .addField('Status', user.presence.status, true)
+
+      .addField('Bot?', user.bot, true)
+
+      .addField('Account created at', user.createdAt, false)
+
+      .setThumbnail(user.displayAvatarURL)
+
+      .setTimestamp()
+
+      .setFooter(`Requested by: ${message.author.username}`, message.author.avatarURL);
+
+
+
+      if(message.content.startsWith (">userinfo")) {
+
+      message.channel.send(embedcoins)
+
+      } 
+
+  });
 
 
 
