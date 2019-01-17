40881c4df591ffcb7ccb7a227a3fb77ac8b71f99
FarjonaCraft <38296873+FarjonaCraft@users.noreply.github.com>
2019-01-17 18:41:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const Discord = require("discord.js")
 
+
+
 const config = require("./config.json")
+
+
 
 const bot = new Discord.Client();
 
+
+
 const fs = require("fs");
+
+
 
 const ms = require("ms");
 
+
+
 bot.commands = new Discord.Collection();
+
+
+
+
 
 
 
@@ -16,93 +30,185 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
+
+
+
+
   if(err) console.log(err);
+
+
+
+
 
 
 
   let jsfile = files.filter(f => f.split(".").pop() === "js");
 
+
+
   if(jsfile.length <= 0){
+
+
 
     console.log("Couldn't find commands.");
 
+
+
     return;
+
+
 
   }
 
 
 
+
+
+
+
 jsfile.forEach((f, i) =>{
+
+
 
   let props = require(`./commands/${f}`);
 
+
+
   console.log(`${f} loaded!`);
+
+
 
   bot.commands.set(props.help.name, props);
 
+
+
 });
 
 
 
+
+
+
+
 });
+
+
+
+
 
 
 
 let statuses = ['patreon.com/norbit | >help', 'Invite me for more | >invite'];
 
+
+
 bot.on("ready", async () => {
+
+
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
+
+
     setInterval(function() {
+
+
 
       let status = statuses[Math.floor(Math.random()*statuses.length)];
 
+
+
       bot.user.setPresence({ game: { name: status}, status: 'online'});
+
+
 
       bot.user.setPresence({ activity: { name: status}, status: 'online'});
 
+
+
     }, 10000)
 
+
+
   
+
+
 
       let user = message.mentions.users.first() || message.author;
 
 
 
+
+
+
+
       if(!user) return message.channel.send("User is not defined in this channel!");
+
+
 
       let embedcoins = new Discord.RichEmbed()
 
+
+
       .setAuthor(`${user.tag}'s Information`)
 
+
+
       .setThumbnail(user.displayAvatarURL)
+
+
 
       .setColor(`#5eff98`)
 
+
+
       .addField('Username', user.username, true)
+
+
 
       .addField('Discrim', user.discriminator, true)
 
+
+
       .addField('Status', user.presence.status, true)
+
+
 
       .addField('Bot?', user.bot, true)
 
+
+
       .addField('Account created at', user.createdAt, false)
+
+
 
       .setThumbnail(user.displayAvatarURL)
 
+
+
       .setTimestamp()
+
+
 
       .setFooter(`Requested by: ${message.author.username}`, message.author.avatarURL);
 
 
 
+
+
+
+
       if(message.content.startsWith (">userinfo")) {
+
+
 
       message.channel.send(embedcoins)
 
+
+
       } 
+
+
 
   });
 
@@ -110,37 +216,77 @@ bot.on("ready", async () => {
 
 
 
+
+
+
+
+
+
 bot.on("message", async message => {
+
+
 
 const swearWords = ["fuck","tering","gvd","dome","omg","dood","sterf","rip","homo","h0mo","hom0", "kanker", "potver", "fack", "f*ck", "r.i.p.", "kut", "dick", "wtf", "tyfus", "godverdomme", "g0dverd0mme", "g0dverdomme"];
 
+
+
 if( swearWords.some(word => message.content.toLowerCase().includes(word)) ) {
+
+
 
 message.delete();
 
+
+
 }
+
+
 
 });
 
 
 
+
+
+
+
 bot.on("message", async message => {
+
+
+
+
 
 
 
   //a little bit of data parsing/general checks
 
+
+
   if(message.author.bot) return;
+
+
 
   if(message.channel.type === 'dm') return;
 
+
+
   let content = message.content.split(" ");
+
+
 
   let command = content[0];
 
+
+
   let args = content.slice(1);
 
+
+
   let prefix = config.prefix;
+
+
+
+
 
 
 
@@ -148,23 +294,39 @@ bot.on("message", async message => {
 
 
 
+
+
+
+
   if (message.content.startsWith(prefix)) {
+
+
 
   let commandfile = bot.commands.get(command.slice(prefix.length));
 
+
+
   if(commandfile) commandfile.run(bot, message, args);
+
+
 
   }
 
+
+
 })
+
+
 
 {
 
 
 
-
-
 bot.login(process.env.BOT_TOKEN);
+
+
+
+
 
 
 
